6a3f204032f517af10c287e91709b2184a2297db
usman <anwerusman63@gmail.com>
2020-11-15 21:23:25 -0800

    Initial commit

---
+++ app/src/main/java/com/example/assigment2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.assigment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.b1);
+    }
+}
