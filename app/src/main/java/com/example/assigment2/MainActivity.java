3b3256da1009cf90956795a3701fd89835037c7a
usman <anwerusman63@gmail.com>
2020-11-15 22:46:55 -0800

    2nd file of a2

--- app/src/main/java/com/example/assigment2/MainActivity.java
+++ app/src/main/java/com/example/assigment2/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.assigment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //TextView tv;
+   // EditText ed;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.b1);
+        /*tv = findViewById(R.id.tv);
+        //tv.setText("Replaced Text");
+        String value =tv.getText().toString();
+        Toast.makeText(this,value, Toast.LENGTH_SHORT).show();
+*/
+      //  ed= findViewById(R.id/ed);
+
+   public void Buttonclicked(View Object v;
+        v) {
+            Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
